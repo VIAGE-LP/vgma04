961ea810b915cd0185a7026fdd42ab3b2d0963fd
Pickachu Chen <pickachu1006@gmail.com>
2020-07-30 17:40:01 +0800

    20200730_big revision

--- js/all.js
+++ js/all.js
@@ -18,4 +18,12 @@ $(document).ready(function () {
             scrollTop: $('#size').offset().top,
         },900);
     });
+
+    //偵測sns line btn 手機版還是電腦版，連結不同
+    var vw=$(window).width();
+    if (vw <= 768) {
+        $(".line_link").attr("href", "https://bit.ly/2YszXqD");
+    } else {
+        $(".line_link").attr("href", "https://line.me/R/ti/p/%40fhl1857j");
+    }
 });
